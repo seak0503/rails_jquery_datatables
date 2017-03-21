c1a86148bb47ca06da717ac4407e0685e7c6a96a
shyamahira <shyamahira@bbtower.co.jp>
2017-03-21 19:17:56 +0900

    coffeeスクリプトをjqueryに変換

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#events').DataTable({
+    "processing": true, // 処理中の表示
+    "serverSide": true, // サーバサイドへ Ajax するか
+    "ajax": "list", // Ajax の通信先
+    "columns": [ //# 扱うカラムの指定
+      { "data": "id" },
+      { "data": "name" },
+    ]
+  });
+});
--- app/assets/javascripts/events.js.coffee
+++
@@ -1,9 +0,0 @@
-jQuery ->
-  $('#events').dataTable
-    "processing": true, # 処理中の表示
-    "serverSide": true, # サーバサイドへ Ajax するか
-    "ajax": "list", # Ajax の通信先
-    "columns": [ # 扱うカラムの指定
-      { "data": "id" },
-      { "data": "name" },
-    ]
