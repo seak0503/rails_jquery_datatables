16b7f2f248380face3fc0fdb7006d0193329b9fc
shyamahira <shyamahira@bbtower.co.jp>
2017-03-23 16:24:22 +0900

    カラムごとの検索内容をローカルストレージに保管する仕組みを追加

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -4,6 +4,7 @@ $(function () {
   });
   var table = $('#events').DataTable({
     "lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "ALL"]],
+    "stateSave": true,
     "processing": true, // 処理中の表示
     "serverSide": true, // サーバサイドへ Ajax するか
     "ajax": "list", // Ajax の通信先
@@ -13,6 +14,20 @@ $(function () {
       { "data": "event_details" },
     ]
   });
+
+  // Restore state
+  var state = table.state.loaded();
+  if (state) {
+    table.columns().eq(0).each(function (colIdx) {
+      var colSearch = state.columns[colIdx].search;
+      if (colSearch) {
+        $('input', table.column(colIdx).footer()).val(colSearch.search);
+      }
+    });
+    table.draw();
+  }
+
+  // Apply the search
   table.columns().every(function () {
     var that = this;
     $('input', this.footer()).on('keyup change', function () {
